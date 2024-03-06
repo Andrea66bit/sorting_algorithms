963815df4fc8c4adb4c5fdc4d376b3a54a36492f
Andrea66bit <122886054+Andrea66bit@users.noreply.github.com>
2024-03-06 13:07:56 +0200

    Delete alx-frontend-javascript directory

--- alx-frontend-javascript/0x00-ES6_basic/1-block-scoped.js
+++
@@ -1,9 +0,0 @@
-export default function taskBlock(trueorfalse) }
-  const task =  false;
-  const task2 =  true;
-
-  if (trueorfalse)  {
-    return [task, task2];
-  }
-  return [task, task2];
-}
--- alx-frontend-javascript/0x00-ES6_basic/10-loops.js
+++
@@ -1,9 +0,0 @@
-export default function appendToEachArrayValue(array, appendstring) {
-  const arr = [];
-  for (const idx of array) {
-    const value = idx;
-    arr.push(appendingString + value);
-  }
-
-  return arr;
-}
